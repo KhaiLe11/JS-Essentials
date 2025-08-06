4ab0cdcc5e6c541e58f8c0a5cbe10927d9cb167b
KhaiLe11 <ledinhkhai2009@gmail.com>
2025-08-06 19:32:24 -0400

    message

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -3,6 +3,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllTasksBtn = document.getElementById("clearAllTasksBtn");
 
 let tasks = [];
 
@@ -35,8 +36,14 @@ function displayTasks() {
             tasks = tasks.filter(task => !task.completed);
             displayTasks();
         }
+    
+    function clearAllTasks() {
+        tasks = [];
+        displayTasks();
+        }
 
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllTasksBtn.addEventListener("click", clearAllTasks);
 
  displayTasks();
